9937f0f08a326a338e6e6fa373241ae4e195366d
uakci <uakci@uakci.eu>
2020-08-24 03:08:22 +0200

    making the bash be good

--- build/scripts/convert-yaml-docs.sh
+++ build/scripts/convert-yaml-docs.sh
@@ -1,16 +1,23 @@
 #!/usr/bin/env bash
-#
-# 1: INPUT - Directory to find yml data 
-INPUT="$(ls $1[a-z]*.yml)"
-# 2: OUTPUT - Directory to output documents
-OUTPUT="$2"
-# 3: TEMPLATE - Path to template file
-TEMPLATE="$3"
+set -e; shopt -s extglob
 
-for File in $INPUT
-do
-    fullfilename=$(basename -- "$File")
-    filename="${fullfilename%.*}"
-    pandoc --template "$TEMPLATE" -s -V "pagetitle:$filename" -V "title:$filename" -f markdown -o "$OUTPUT$filename.html" "$File"
-    echo "Converted $fullfilename to $OUTPUT$filename.html!"
+if [ $# -ne 3 ]; then
+  tee >&2 <<BAD
+I need more or fewer arguments (3, to be exact):
+${0} [input] [output] [template]
+where:
+  input = directory from which to draw .yml files
+  output = directory to place the output files in (genius)
+  template = template file (.html)
+BAD
+  exit 1
+fi
+
+INPUT="$1" OUTPUT="$2" TEMPLATE="$3"
+mkdir -p "$OUTPUT"
+
+for File in "$INPUT"/!(__*).yml; do
+  filename="$(basename -s .yml -- "$File")"
+  pandoc --template "$TEMPLATE" -s -V "pagetitle:$filename" -V "title:$filename" -f markdown -o "$OUTPUT/$filename.html" "$File"
+  echo "Converted $filename.yml to $OUTPUT/$filename.html!"
 done
